b9f5256f991c0cc46c516891a41ecd825826f0ae
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-03-07 21:32:39 -0300

    Register new user (#6)
    * chore: initial setup and configuration for zsa-react, zsa-react-query, and zsa-openapi
    
    * feat: add API call for creating a new user

---
+++ src/_lib/actions/singup.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+import { createServerAction, ZSAError } from "zsa";
+import { createNewUserSchema } from "../schema/newUser";
+
+export const actionSingUp = createServerAction()
+  .input(createNewUserSchema)
+  .handler(async ({ input: { name, email, password } }) => {
+    const response = await fetch(process.env.BACKEND_URL + "/singup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+      cache: "no-store",
+    }).catch(() => {
+      throw new ZSAError(
+        "ERROR",
+        "Error accessing the new user registration service.",
+      );
+    });
+
+    if (response.status !== 200 && response.status !== 201) {
+      const { message } = await response.json();
+
+      throw new ZSAError("INPUT_PARSE_ERROR", message);
+    }
+
+    revalidateTag("list-users");
+
+    return await response.json();
+  });
